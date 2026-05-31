1df7bc13c72798d5991bea661bf56069e74f61af
Sheikh Hussain <sheikh.hussain1155@gmail.com>
2026-05-31 22:24:44 +0100

    first draft of what I have made the functions to be workable. I haven't used a lot of try and except clauses as I wanted to first get the base functions working and then error handle as I go along.

--- services/cached_data.py
+++ services/cached_data.py
@@ -15,7 +15,6 @@ redis_client = redis.Redis(
 )
 
 
-# Main function that houses all the other functions to work.
 def get_data(url):
     data = get_data_from_redis(url)
     if data == False:
@@ -24,24 +23,21 @@ def get_data(url):
         return json.loads(data)
 
 
-# First look for the data inside redis, if data is < 12 hours, then return:
 def get_data_from_redis(url):
     data = redis_client.get(url)
-    if data:  # response null or error
+    if data:
         return json.loads(data)
     else:
         data = get_data_from_api(url)
         return json.loads(data)
 
 
-# If not available, get the data from the api
 def get_data_from_api(url):
     response = requests.get(url)
     data = response.json()
-    store_data_in_redis(url, data, TIME)
+    store_data_in_redis(url, json.dumps(data, indent=2), TIME)
     return json.loads(data)
 
 
-# Update the data in the redis database and set expiration to being 12 hours
 def store_data_in_redis(key, value, expiration):
-    pass
+    redis_client.set(key, value, ex=expiration)
