d76a5f732ec85949533417bf6936df374accaf6f
Sheikh Hussain <sheikh.hussain1155@gmail.com>
2026-05-31 23:40:48 +0100

    I used a bit of chatGPT to find what the errors were on why it wasn't bringing back the results though the functions seemed to be right. After making a few tweaks. it all seems to work now. I just need to now include a limiter for the requests and also use some error handling

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, request
 from flask_limiter import Limiter
 from dotenv import load_dotenv
-import requests
 import os
-import jsonify
-import redis
 from services.cached_data import get_data
 
 load_dotenv()
@@ -21,20 +18,20 @@ base_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/se
 @app.route("/weather", methods=["GET"])
 def weather():
     location = request.args.get("location")
-    date_from = request.args.get("date_from")
-    date_to = request.args.get("date_to")
+    date_from = request.args.get("datefrom")
+    date_to = request.args.get("dateto")
 
     if location and date_from and date_to:
         url = f"{base_url}{location}/{date_from}/{date_to}?unitGroup=uk&key={API_KEY}&contentType=json"
-        get_data(url)
+        return get_data(url)
     elif location and date_from:
         url = f"{base_url}{location}/{date_from}?unitGroup=uk&key={API_KEY}&contentType=json"
-        get_data(url)
+        return get_data(url)
     elif location:
         url = f"{base_url}{location}/?unitGroup=uk&key={API_KEY}&contentType=json"
-        get_data(url)
+        return get_data(url)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, port=port, host="127.0.0.1")
+    app.run(debug=True, port=port)
--- services/cached_data.py
+++ services/cached_data.py
@@ -1,46 +1,44 @@
 import redis
 import requests
 import json
-import jsonify
 import os
 from dotenv import load_dotenv
+from flask import jsonify
 
 load_dotenv()
 
-TIME = int(os.environ.get("CACHE_EXPIRATION"))
+TIME = 43200
 
 redis_client = redis.Redis(
     host=os.environ.get("REDIS_HOST"),
     port=int(os.environ.get("REDIS_PORT")),
     db=int(os.environ.get("REDIS_DB")),
+    decode_responses=True,
 )
 
 
 def get_data(url):
     data = get_data_from_redis(url)
-    if data == False:
-        get_data_from_api(url)
-    else:
+    if data:
         return jsonify(data)
+    else:
+        data = get_data_from_api(url)
+    return jsonify(data)
 
 
 def get_data_from_redis(url):
     data = redis_client.get(url)
     if data:
-        return jsonify(data)
-    else:
-        data = get_data_from_api(url)
-        return jsonify(data)
+        return json.loads(data)
+    return None
 
 
 def get_data_from_api(url):
-    try:
-        response = requests.get(url)
-        data = response.json()
-        store_data_in_redis(url, json.dumps(data, indent=2), TIME)
-    except Exception as e:
-        print(e)
-    return jsonify(data)
+    response = requests.get(url)
+    data = response.json()
+    response.raise_for_status()
+    store_data_in_redis(url, json.dumps(data, indent=2), TIME)
+    return data
 
 
 def store_data_in_redis(key, value, expiration):
