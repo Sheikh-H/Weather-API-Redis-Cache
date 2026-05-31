b0a951664c4a5d1aaa99a118b7ca2a09e96c2eee
Sheikh Hussain <sheikh.hussain1155@gmail.com>
2026-06-01 00:08:04 +0100

    After going to ChatGPT to learn some stuff about rate limiters and which one to use as there are a couple of different ones I have seen. I embedded the rate limiter from the flask library as this is said to be what works best with Flask. I haven't really used limiters before but I do get how this function works and what it does. there a few properties that we could include but it makes more sense for me to just design the code as I have with minimal AI as possible.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from dotenv import load_dotenv
+from urllib.parse import quote
 import os
 from services.cached_data import get_data
 
@@ -9,27 +11,33 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
 app = Flask(__name__)
+limiter = Limiter(
+    key_func=get_remote_address,
+    app=app,
+)
 
 base_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
 
-# url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/manchester/2026-05-31/2026-06-15?unitGroup=uk&key=KWGGLKXSLHEDMHADL9GWHBUWD&contentType=json"
-
 
 @app.route("/weather", methods=["GET"])
+@limiter.limit("50 per hour")
 def weather():
     location = request.args.get("location")
+    location = quote(location)
     date_from = request.args.get("datefrom")
     date_to = request.args.get("dateto")
 
+    if not location:
+        return {"error": "Please enter a location"}, 400
     if location and date_from and date_to:
         url = f"{base_url}{location}/{date_from}/{date_to}?unitGroup=uk&key={API_KEY}&contentType=json"
-        return get_data(url)
+        return get_data(url), 200
     elif location and date_from:
         url = f"{base_url}{location}/{date_from}?unitGroup=uk&key={API_KEY}&contentType=json"
-        return get_data(url)
+        return get_data(url), 200
     elif location:
         url = f"{base_url}{location}/?unitGroup=uk&key={API_KEY}&contentType=json"
-        return get_data(url)
+        return get_data(url), 200
 
 
 if __name__ == "__main__":
